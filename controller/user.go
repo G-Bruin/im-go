76c48f1f361ca253ff1f07b274b3c2bdeab7f6c0
yuxiaolong <yuxl@kongdao.me>
2019-07-27 14:52:06 +0800

    feat: add govalidator

--- controller/user.go
+++ controller/user.go
@@ -1,34 +1,17 @@
 package controller
 
 import (
-	valid "github.com/asaskevich/govalidator"
+	"im-go/middleware/validator"
 	"net/http"
 )
 
-type Post struct {
-	Title string `valid:"alphanum,required,stringlength(2|7)"`
-	//Message  string `valid:"duck,ascii"`
-	//Message2 string `valid:"animal(dog)"`
-	AuthorIP string `valid:"ipv4"`
-	Date     string `valid:"-"`
-}
-
 func Register(w http.ResponseWriter, request *http.Request) {
 	request.ParseForm()
-	//name := request.PostForm.Get("name")
-	//password := request.PostForm.Get("password")
-
-	post := &Post{
-		Title: "MyEx6",
-		//Message:  "duck",
-		//Message2: "dog",
-		AuthorIP: "123.234.54.3",
+	postData := validator.User{
+		Name:     request.PostForm.Get("name"),
+		Password: request.PostForm.Get("password"),
 	}
-
-	result, err := valid.ValidateStruct(post)
-	if err != nil {
-		println(" " + err.Error())
-	}
-	println(result)
+	//前置数据验证
+	postData.RegisterValidator(w)
 
 }
